c63e0d9b7d2c6d2527bf9e903469b5fb9e7cbf94
Gregor Alpers <gregoralpers@Gregors-MacBook-Pro-8690.local>
2022-03-30 09:53:50 +0100

    Drafting

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "push_swap.h"
 
-static void    sort(int ac, char **av)
+static void	sort(int ac, char **av)
 {
         t_stack *a;
         t_stack *b;
@@ -8,9 +8,9 @@ static void    sort(int ac, char **av)
         a = initialize_stack(ac, av, 0);
         if (a && ft_lstsize(a) > 1 && check_repeats(a, 0))
         {
-            b = NULL;
-            if (ft_lst_size (a) <= 3)
-                sort_three(&a);
+    		b = NULL;
+        	if (ft_lst_size (a) <= 3)
+        		sort_three(&a);
         }
         ft_lstclear(&a, NULL);
         ft_lstclear(&b, NULL);
